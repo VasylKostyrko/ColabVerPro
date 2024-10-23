06d34002fcf1a7dfa21bf0d25b396462fe158607
VasylKostyrko <crocodile21@gmail.com>
2024-10-23 12:47:43 +0300

    Initial

---
+++ examples/exCheckAnProg.py
@@ -0,0 +1,12 @@
+from verification import check_anfun
+
+dictcppos = {}
+cpdictconds = {}
+progstru = []
+dicttermexpr = {}
+dict_var_types_cp = {}
+curdir= ""
+program = "frac_fun_term_area"  # Ім'я аналізованої програми
+exdir = "anprograms"  # Ім'я каталогу аналізованих програм
+filename = curdir + "\\" + exdir + "\\" + program + ".py"
+res = check_anfun(filename, dictcppos, cpdictconds, progstru, dicttermexpr, dict_var_types_cp)
