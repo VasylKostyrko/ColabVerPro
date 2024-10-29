70af05a181dbfff834bb0595e4d2eb615c681249
VasylKostyrko <crocodile21@gmail.com>
2024-10-29 19:16:40 +0200

    errMsg

--- verification/show_info.py
+++ verification/show_info.py
@@ -1,2 +1,10 @@
+def findErrMsg(lang, errCode):
+    """Відображає повідомлення про помилку!"""
+    errDictMsg = messLang[errCode]
+    errMsg = errDictMsg[lang]
+    return errMsg
+
 def viewErrMsg(errMsg):
+    """Відображає повідомлення про помилку
+    в підвалі клітинки"""
 	print(errMsg)
