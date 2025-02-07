0c7a000de58f4a5e0e61e6671c2095aa8f81f180
Joe Marvell <jmarvell@ukr.net>
2025-02-07 15:32:34 +0200

    Update5

--- examples/exLangMess.py
+++
@@ -1,5 +0,0 @@
-curdir= ""
-fname = curdir + "verification/" + "langMess.json"
-with open(fname, 'r', encoding='utf-8') as file_object:
-    messDict = json.load(file_object)
-print(messDict["noCPCond"])
